b851a5ef72cf30c4394882d5ec74f7781e9a2c08
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-04-29 12:30:53 +0100

    gcd

---
+++ 1.2/gcd.scm
@@ -0,0 +1,12 @@
+#lang scheme
+
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+; (gcd 206 40)
+; (gcd 40 (remainder 206 40))
+; (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+; (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+; (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (rem (rem) (rem rem)
