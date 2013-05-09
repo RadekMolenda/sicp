e7b457c0df955b36a187dda9ccdc70977dee1c99
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-05-09 21:53:07 +0100

    Dont use nested defines

--- 1.3/simpsons-rule.scm
+++ 1.3/simpsons-rule.scm
@@ -5,13 +5,13 @@
 (provide integral)
 
 (define (integral f a b n)
-  (define h (/ (- b a) n))
-  (define (inc x) (+ x 1))
-  (define (transform i)
-    (cond
-      ((= i 0) (f a))
-      ((= i n) (f (+ a (* i h))))
-      ((even? i) (* 2 (f (+ a (* i h)))))
-      ((odd? i) (* 4 (f (+ a (* i h)))))))
-  (* (/ h 3) (sum transform 0 inc n)))
-
+  (let*
+    ((h (/ (- b a) n))
+    (inc (lambda (x) (+ x 1)))
+    (transform (lambda (i)
+      (cond
+        ((= i 0) (f a))
+        ((= i n) (f (+ a (* i h))))
+        ((even? i) (* 2 (f (+ a (* i h)))))
+        ((odd? i) (* 4 (f (+ a (* i h)))))))))
+  (* (/ h 3) (sum transform 0 inc n))))
