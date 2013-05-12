9ca308366af71c18064abd049d37c3961855f324
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-05-12 22:03:15 +0100

    Introduce iterative #product

--- sicp/1.3/product.scm
+++ sicp/1.3/product.scm
@@ -1,8 +1,16 @@
 #lang scheme
 
-(provide product)
+(provide product
+         producti)
 
 (define (product term a next b)
   (if (> a b)
     1
     (* (term a) (product term (next a) next b))))
+
+(define (producti term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (* (term a) result))))
+  (iter a 1))
--- sicp/1.3/product_test.scm
+++ sicp/1.3/product_test.scm
@@ -5,9 +5,16 @@
 (require "product.scm")
 
 (define (inc x) (+ x 1))
+(define (inc-by-two x) (inc (inc x)))
 (define (identity x) x)
 
 (check-expect (product identity 1 inc 3) 6)
 (check-expect (product identity 1 inc 4) 24)
+(check-expect (product identity 1 inc-by-two 4) 3)
+
+(check-expect (producti identity 1 inc 3) 6)
+(check-expect (producti identity 1 inc 4) 24)
+(check-expect (producti identity 1 inc-by-two 4) 3)
+
 
 (test)
