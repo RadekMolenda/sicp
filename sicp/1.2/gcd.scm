b36ee5e1df76daaa176135b9a87470471ef1085d
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-05-13 15:20:01 +0100

    Don't leave comments in code

--- sicp/1.2/gcd.scm
+++ sicp/1.2/gcd.scm
@@ -4,9 +4,3 @@
   (if (= b 0)
     a
     (gcd b (remainder a b))))
-
-; (gcd 206 40)
-; (gcd 40 (remainder 206 40))
-; (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
-; (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
-; (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (rem (rem) (rem rem)
