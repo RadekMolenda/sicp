b81b8ccd9971a1f39d027f727d14a669401efcc1
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-05-14 21:51:24 +0100

    Implement #verbose-fixed-point

--- sicp/1.3/fixed-points-test.scm
+++ sicp/1.3/fixed-points-test.scm
@@ -8,4 +8,14 @@
 
 (check-within (my-sqrt 3) (sqrt 3) tolerance)
 
+(display "without damping")
+(newline)
+(verbose-fixed-point (lambda (x) (/ (log 1000) (log x)))
+                     2.0)
+(newline)
+(display "with damping")
+(newline)
+(verbose-fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
+                     2.0)
+
 (test)
--- sicp/1.3/fixed-points.scm
+++ sicp/1.3/fixed-points.scm
@@ -2,11 +2,25 @@
 
 (provide tolerance
          fixed-point
-         my-sqrt)
+         my-sqrt
+         verbose-fixed-point
+         average)
 
 (define (average x y) (/ (+ x y) 2))
 
 (define tolerance 0.00001)
+
+(define (verbose-fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs(- v1 v2)) tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
 
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2)
