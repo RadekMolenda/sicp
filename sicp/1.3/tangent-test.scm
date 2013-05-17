82fd138bfe9779233114952bf255eb984bf99ae3
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-05-17 15:35:53 +0100

    Implement 'raw' tangent method

--- sicp/1.3/tangent-test.scm
+++ sicp/1.3/tangent-test.scm
@@ -8,4 +8,7 @@
 (check-within (tan-cf (/ pi 12) 10) (tan (/ pi 12)) 0.0001)
 (check-within (tan-cf (/ pi 5) 10) (tan (/ pi 5)) 0.0001)
 
+(check-within (tan-cf-raw (/ pi 5) 10) (tan (/ pi 5)) 0.0001)
+
+
 (test)
--- sicp/1.3/tangent.scm
+++ sicp/1.3/tangent.scm
@@ -1,9 +1,19 @@
 #lang scheme
 
-(provide tan-cf)
+(provide tan-cf
+         tan-cf-raw)
 (require "continued-fraction.scm")
 
 (define (tan-cf x k)
   (define (n a) (if (= a 1) x (- 0 (* x x))))
   (define (d a) (- (* 2 a) 1))
   (cont-frac-i n d k))
+
+(define (tan-cf-raw x k)
+  (define (n a) (if (= a 1) x (- 0 (* x x))))
+  (define (iter a acc)
+    (if (= a 0)
+      acc
+      (iter (- a 1) (/ (n a) (+ (- (* 2 a) 1) acc)))))
+  (iter k (/ (n k) (- (* 2 k) 1))))
+
