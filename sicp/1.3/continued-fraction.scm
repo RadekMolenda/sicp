810a79b6c1584d21dfd35c159d3ed30894c22a42
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-05-17 15:27:54 +0100

    Dont use redundant variables

--- sicp/1.3/continued-fraction.scm
+++ sicp/1.3/continued-fraction.scm
@@ -7,14 +7,12 @@
   (define (y a)
     (if (= a k)
       (/ (n a) (d a))
-      (let ((next (+ a 1)))
-        (/ (n a) (+ (d a) (y next))))))
+      (/ (n a) (+ (d a) (y (+ a 1))))))
   (y 1))
 
 (define (cont-frac-i n d k)
   (define (y a acc)
     (if (= a 0)
       acc
-      (let ((next (- a 1)))
-        (y next (/ (n a) (+ (d a) acc))))))
+      (y (- a 1) (/ (n a) (+ (d a) acc)))))
   (y k (/ (n k) (d k))))
